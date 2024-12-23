83ee6e6658d42102067bef35a8903c446c59007a
Jayden Chan <jayden@jayden.codes>
2024-12-23 12:32:38 -0700

    build: don't use devenv
    Slows down the nix build and it's not necessary

--- flake.nix
+++ flake.nix
@@ -3,16 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    devenv.url = "github:cachix/devenv";
     crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
-    fenix.url = "github:nix-community/fenix";
-    fenix.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
   };
 
   outputs =
@@ -21,9 +13,8 @@
       nixpkgs,
       crane,
       flake-utils,
-      devenv,
       ...
-    }@inputs:
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
@@ -76,40 +67,31 @@
           inherit sensors-mon;
         };
 
-        packages.devenv-up = self.devShells.${system}.default.config.procfileScript;
-        packages.devenv-test = self.devShells.${system}.default.config.test;
-
         packages.default = sensors-mon;
 
         apps.default = flake-utils.lib.mkApp { drv = sensors-mon; };
 
-        devShells.default = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [
-            (
-              { pkgs, config, ... }:
-              {
-                packages = [ pkgs.lm_sensors ];
-
-                env = {
-                  LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-                  LMSENSORS_LIB_DIR = "${pkgs.lm_sensors}/lib";
-                };
-
-                languages.rust.enable = true;
-                languages.rust.channel = "stable";
-
-                enterShell = ''
-                  export BINDGEN_EXTRA_CLANG_ARGS="$(< ${pkgs.stdenv.cc}/nix-support/libc-crt1-cflags) \
-                    $(< ${pkgs.stdenv.cc}/nix-support/libc-cflags) \
-                    $(< ${pkgs.stdenv.cc}/nix-support/cc-cflags) \
-                    $(< ${pkgs.stdenv.cc}/nix-support/libcxx-cxxflags) \
-                    ${pkgs.lib.optionalString pkgs.stdenv.cc.isClang "-idirafter ${pkgs.stdenv.cc.cc}/lib/clang/${pkgs.lib.getVersion pkgs.stdenv.cc.cc}/include"} \
-                    ${pkgs.lib.optionalString pkgs.stdenv.cc.isGNU "-isystem ${pkgs.stdenv.cc.cc}/include/c++/${pkgs.lib.getVersion pkgs.stdenv.cc.cc} -isystem ${pkgs.stdenv.cc.cc}/include/c++/${pkgs.lib.getVersion pkgs.stdenv.cc.cc}/${pkgs.stdenv.hostPlatform.config} -idirafter ${pkgs.stdenv.cc.cc}/lib/gcc/${pkgs.stdenv.hostPlatform.config}/${pkgs.lib.getVersion pkgs.stdenv.cc.cc}/include"}"
-                '';
-              }
-            )
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+          packages = [
+            pkgs.lm_sensors
+            pkgs.rust-analyzer
+            pkgs.rustfmt
           ];
+
+          env = {
+            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+            LMSENSORS_LIB_DIR = "${pkgs.lm_sensors}/lib";
+          };
+
+          shellHook = ''
+            export BINDGEN_EXTRA_CLANG_ARGS="$(< ${pkgs.stdenv.cc}/nix-support/libc-crt1-cflags) \
+              $(< ${pkgs.stdenv.cc}/nix-support/libc-cflags) \
+              $(< ${pkgs.stdenv.cc}/nix-support/cc-cflags) \
+              $(< ${pkgs.stdenv.cc}/nix-support/libcxx-cxxflags) \
+              ${pkgs.lib.optionalString pkgs.stdenv.cc.isClang "-idirafter ${pkgs.stdenv.cc.cc}/lib/clang/${pkgs.lib.getVersion pkgs.stdenv.cc.cc}/include"} \
+              ${pkgs.lib.optionalString pkgs.stdenv.cc.isGNU "-isystem ${pkgs.stdenv.cc.cc}/include/c++/${pkgs.lib.getVersion pkgs.stdenv.cc.cc} -isystem ${pkgs.stdenv.cc.cc}/include/c++/${pkgs.lib.getVersion pkgs.stdenv.cc.cc}/${pkgs.stdenv.hostPlatform.config} -idirafter ${pkgs.stdenv.cc.cc}/lib/gcc/${pkgs.stdenv.hostPlatform.config}/${pkgs.lib.getVersion pkgs.stdenv.cc.cc}/include"}"
+          '';
         };
       }
     );
